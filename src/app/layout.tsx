943217fde812ca7793c9c925e17e6598ba332f13
Jaaseia Gian Abenoja <jaaseiacocabenoja@gmail.com>
2025-12-13 14:13:52 +0800

    fix: add NextTopLoader for improved loading experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { SessionManager } from "@/components/auth/shared/SessionManager";
 import { Toaster } from "sonner";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "RaceTrackr",
@@ -16,6 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NextTopLoader
+          color="#fc4c02"
+          height={3}
+          showSpinner={false}
+          shadow="0 0 10px #fc4c02,0 0 5px #fc4c02"
+        />
         <SessionManager>
           {children}
         </SessionManager>
